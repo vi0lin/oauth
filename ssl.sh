c2da79736e68a7095fb9fe712f8c893568bac8a5
vi0lin <codekowski@gmail.com>
2026-04-03 23:07:33 +0200

    Evaluate

--- ssl.sh
+++ ssl.sh
@@ -8,21 +8,21 @@ generate_ssl() {
   def="-addext \"subjectAltName = IP:$ip\""
   all="$abc $def"
   command="openssl req -newkey rsa:2048 -new -nodes -keyout ${p}key.pem -out ${p}csr.pem $all"
-  echo $command
+  eval $command
   command="openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout ${p}key.pem -out ${p}cert.pem $all"
-  echo $command
+  eval $command
   # https://stackoverflow.com/questions/21297139/how-do-you-sign-a-certificate-signing-request-with-your-certification-authority
   # Generate a self-signed signing certificate
   command="openssl req -x509 -days 365 -key ${p}key.pem -out ${p}ca_cert.pem $all"
-  echo $command
+  eval $command
   # Move To App
   # cp ${p}ca_cert.pem {location}cert.pem
   # Generate a certificate request
   command="openssl req -new -key ${p}key.pem -out ${p}cert_req.pem $all"
-  echo $command
+  eval $command
   # Generate a signed certificate
   command="openssl x509 -req -in ${p}cert_req.pem -days 365 -CA ${p}ca_cert.pem -CAkey ${p}key.pem -CAcreateserial -out ${p}signed_cert.pem"
-  echo $command
+  eval $command
   # Move To App
   # cp ${p}signed_cert.pem {path}
   echo done
